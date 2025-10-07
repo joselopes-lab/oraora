3aa51448e95c7de9d9207e734896dc4d8bedb199
Jose Filho <jose.lopes@itaipuparquetec.org.br>
2025-10-07 08:09:21 -0300

    adicionando novas credenciais

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,23 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyBSRvXjw8-5b_AbHQVGsZdoHUuOIFevHtc",
-  authDomain: "oraora.com.br",
-  projectId: "secureadminpanel",
-  storageBucket: "secureadminpanel.appspot.com",
-  messagingSenderId: "184982072878",
-  appId: "1:184982072878:web:5d651f2df6ae1964e4f9bf"
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyBSRvXjw8-5b_AbHQVGsZdoHUuOIFevHtc",
+//   authDomain: "oraora.com.br",
+//   projectId: "secureadminpanel",
+//   storageBucket: "secureadminpanel.appspot.com",
+//   messagingSenderId: "184982072878",
+//   appId: "1:184982072878:web:5d651f2df6ae1964e4f9bf"
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrs0lBvav44t_pGHmzRD0XVsdsuHHSA6s",
+  authDomain: "oraora-45adb.firebaseapp.com",
+  projectId: "oraora-45adb",
+  storageBucket: "oraora-45adb.firebasestorage.app",
+  messagingSenderId: "645528171911",
+  appId: "1:645528171911:web:78cfa11e4ac41ac2f60f22",
+  measurementId: "G-ZS6T2F8VZM"
 };
 
 
